cbff11bbf585829b5ed527c45331a5a5afe21f4d
Brian Moriartty <morbrian@me.com>
2018-03-04 08:01:05 -0800

    backrest interactive restore pod

--- apps/init-pg-templates.sh
+++ apps/init-pg-templates.sh
@@ -8,6 +8,7 @@ oc create -n openshift -f ${WORKSPACE}/templates/pg-cluster-vols.yaml
 oc create -n openshift -f ${WORKSPACE}/templates/pg-primary.yaml
 oc create -n openshift -f ${WORKSPACE}/templates/pg-replica.yaml
 oc create -n openshift -f ${WORKSPACE}/templates/pg-backrest-restore.yaml
+oc create -n openshift -f ${WORKSPACE}/templates/pg-backrest-interactive.yaml
 
 # if template needs to be reloaded, it can be force replaced:
 # oc replace --force -n openshift -f templates/filename.yaml
