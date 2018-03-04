7293c8d8beb1395e448d94531ac62198c6800616
Brian Moriartty <morbrian@me.com>
2018-03-04 15:48:48 -0800

    gh-#3 initial cut of pgpool
    Works with simple psql test, but there are repeated errors in the logs
    and the pool_passwd file is unconfigured after deployment.

--- apps/init-pg-configmap.sh
+++ apps/init-pg-configmap.sh
@@ -2,9 +2,16 @@
 
 WORKSPACE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "Create ConfigMap from pgconf files"
+echo "Create pgconf ConfigMap from pgconf files for postgres"
 oc create configmap pgconf \
     --from-file=pgconf=${WORKSPACE}/pgconf/postgresql.conf \
     --from-file=pghba=${WORKSPACE}/pgconf/pg_hba.conf \
     --from-file=setup=${WORKSPACE}/pgconf/setup.sql \
     --from-file=pgbackrest=${WORKSPACE}/pgconf/pgbackrest.conf
+
+echo "Create pgpoolconf ConfigMap from pgpool files for pgpool"
+oc create configmap pgpoolconf \
+    --from-file=pgpool=${WORKSPACE}/pgpool/pgpool.conf \
+    --from-file=poolhba=${WORKSPACE}/pgpool/pool_hba.conf \
+    --from-file=poolpasswd=${WORKSPACE}/pgpool/pool_passwd
+
